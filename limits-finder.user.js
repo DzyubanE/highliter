852fcda0fb39f6f3c2ddd3f9e98596ea2589dfcc
DzyubanE <dzyuban.e@mailbox366.com>
2026-04-22 14:45:39 +0300

    beta release 1.0.3

--- limits-finder.user.js
+++ limits-finder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Limit's Finder Team B BETA
 // @namespace    team-bestie
-// @version      1.0.2
+// @version      1.0.3
 // @updateURL    https://github.com/DzyubanE/MENA-L2/raw/refs/heads/main/limits-finder.user.js
 // @downloadURL  https://github.com/DzyubanE/MENA-L2/raw/refs/heads/main/limits-finder.user.js
 // @author       You
@@ -9,7 +9,7 @@
 // @match        https://th-managment.com/en/admin/backoffice/paymentsupport*
 // @match        https://my-managment.com/en/admin/backoffice/paymentsupport*
 // @match        https://managment.io/en/admin/backoffice/paymentsupport*
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=th-managment.com
+// @icon         https://github.com/DzyubanE/MENA-L2/raw/refs/heads/main/limits-finder.svg
 // @grant        GM_xmlhttpRequest
 // @connect      doc.office.lan
 // ==/UserScript==
